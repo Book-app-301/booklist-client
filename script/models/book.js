39e3c5063b15c043f99280c446bc2dedbdf0139d
Dawn Aldrich <dawnstarr.aldrich@gmail.com>
2018-02-28 09:43:23 -0800

    switching drivers

--- script/models/book.js
+++ script/models/book.js
@@ -4,29 +4,32 @@ var app={};
 var __API_URL__ = 'https://jr-da-booklist.herokuapp.com';
 
 (function (module) {
-    function errorCallback(err) {
-      console.error(err);
-      module.errorView.initErrorPage(err);
-    }
+  function errorCallback(err) {
+    console.error(err);
+    module.errorView.initErrorPage(err);
+  }
 
-function Book(bookObject){
+  function Book(bookObject){
     Object.keys(bookObject).forEach(key => this[key] = bookObject[key]);
-}
+  }
 
-Book.prototype.toHtml = function(){
+  Book.prototype.toHtml = function(){
     let template = Handlebars.compile($('#book-template').text());
     return template(this);
-}
+  }
 
-Book.all = [];
+  Book.all = [];
 
-Book.loadAll = rows => {
-  Book.all = rows.sort((a, b) => b.title - a.title).map(book => new Book(book));
-}
+  Book.loadAll = rows => {Book.all = rows.sort((a, b) => b.title - a.title).map(book => new Book(book));}
 
-Book.fetchAll = callback =>
-$.get(`${_API_URL_}/books`)
-    .then(Books.loadAll)
-    .then(callback)
-    .catch(errorCallback);
+  //look at server .js at API endpoints for query
+  Book.fetchAll = callback =>
+    //$.get(`${_API_URL_}/books`)
+    $.get(`${__API_URL__}/api/vi/books`)
+      .then(Book.loadAll)
+      .then(callback)
+      .catch(errorCallback);
+  module.Book = Book;
+
+})(app);
 
