bd27b89082326adde443b2297b83df156dccc345
Dawn Aldrich <dawnstarr.aldrich@gmail.com>
2018-03-01 13:35:11 -0800

    finished step 1 thru 3

--- script/models/book.js
+++ script/models/book.js
@@ -27,6 +27,18 @@ var __API_URL__ = 'http://localhost:3000';
       .then(Book.loadAll)
       .then(callback)
       .catch(errorCallback);
+
+  Book.fetchOne = callback =>
+    $.get(`${__API_URL__}/api/v1/books/${ctx.params.book_id}`)
+      .then(results=>ctx.book = results[0])
+      .then(callback)
+      .catch(errorCallback);
+
+  Book.create = book =>
+    $.post(`${__API_URL__}/api/v1/books`,book)
+      .then(results=>ctx.book = results[0])
+      .then(()=>page('/'))
+      .catch(errorCallback);
   module.Book = Book;
 
 })(app);
